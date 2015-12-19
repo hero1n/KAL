527fe5297b0c26dee571af73961b5ff438733343
노재원 <opqw987@gmail.com>
2015-12-19 16:12:54 +0900

    페이지컨텐츠 컨트롤러 추가

--- KAL/PageContentViewController.h
+++ KAL/PageContentViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  PageContentViewController.h
 //  KAL
 //
 //  Created by 노재원 on 2015. 12. 19..
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface PageContentViewController : UIViewController
 
 @end
-
---
+++ KAL/PageContentViewController.m
@@ -0,0 +1,22 @@
+//
+//  PageContentViewController.m
+//  KAL
+//
+//  Created by 노재원 on 2015. 12. 19..
+//  Copyright © 2015년 heroin. All rights reserved.
+//
+
+#import "PageContentViewController.h"
+
+@interface PageContentViewController ()
+
+@end
+
+@implementation PageContentViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
