c43c8e8e32d987c7df3255bb3182327b831af580
노재원 <opqw987@gmail.com>
2015-12-19 16:06:34 +0900

    Initial Commit

---
+++ KAL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KAL
+//
+//  Created by 노재원 on 2015. 12. 19..
+//  Copyright © 2015년 heroin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KAL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KAL
+//
+//  Created by 노재원 on 2015. 12. 19..
+//  Copyright © 2015년 heroin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
